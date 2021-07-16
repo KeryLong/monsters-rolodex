847a7a6bafb02f920e50d06e668d3c2639c5e199
KeryLong <kery.long@gmail.com>
2021-07-16 11:14:26 -0600

    Up to filtering state

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()
+    .then(users =>this.setState({ monsters: users }))
+    );
+  }
+  render() {
+    const {monsters, searchField} = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+    return (
+      <div className="App">
+        <input type = 'search' placeholder='search monsters' 
+        onChange={filter => {
+          this.setState({ searchField: filter.target.value }, () =>
+          console.log(this.state)
+            )
+        }}
+        />
+          
+        <CardList monsters={filteredMonsters} /> 
+       </div>
+            
+    )
+  }
+}
+
+
+export default App;
