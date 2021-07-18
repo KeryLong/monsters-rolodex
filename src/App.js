1e9b0dfc01e8ac7b5ddb37c5ab79737588bf9d69
Kery Development <kerylong@MacBook-Pro.hitronhub.home>
2021-07-18 12:32:07 -0600

    up to binding in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -10,26 +11,31 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json()
-    .then(users =>this.setState({ monsters: users }))
-    );
+    .then(response => response.json())
+    .then(users =>this.setState({ monsters: users }));
   }
+    
+  
+      handleChange(e){
+      this.setState({ searchField: e.target.value });
+    }
+      
   render() {
     const {monsters, searchField} = this.state
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
     return (
       <div className="App">
-        <input type = 'search' placeholder='search monsters' 
-        onChange={filter => {
-          this.setState({ searchField: filter.target.value }, () =>
-          console.log(this.state)
-            )
-        }}
+       
+        <SearchBox        
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+      
         />
           
         <CardList monsters={filteredMonsters} /> 
@@ -38,6 +44,5 @@ class App extends Component {
     )
   }
 }
-
 
 export default App;
